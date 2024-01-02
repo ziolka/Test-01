fd8598bd800accec22e7a0f7c1c119d6b34303a7
ziolka <149949482+ziolka@users.noreply.github.com>
2024-01-02 17:15:20 +0100

    Zadanie domowe - moduł 6

---
+++ create_folder_structure.py
@@ -0,0 +1,20 @@
+import os
+
+def create_folder_structure(path):
+    os.chdir(path)
+    if not os.path.exists('images'):
+        os.mkdir('images')
+    if not os.path.exists('movies'):
+        os.mkdir('video')
+    if not os.path.exists('documents'):
+        os.mkdir('documents')
+    if not os.path.exists('music'):
+        os.mkdir('audio')
+    if not os.path.exists('archives'):
+        os.mkdir('archives')
+    if not os.path.exists('others'):
+        os.mkdir('others')
+
+
+if __name__ == '__main__':
+    create_folder_structure('D:/Python/Bałagan')
